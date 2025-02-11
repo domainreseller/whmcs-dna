d99c1e450616fdd8bfb5694e3f6c14d3e24b0732
xa81 <poisonkid@gmail.com>
2025-02-12 02:42:55 +0300

    v2.2.3

--- modules/registrars/domainnameapi/domainnameapi.php
+++ modules/registrars/domainnameapi/domainnameapi.php
@@ -2,7 +2,7 @@
 /**
  * Module WHMCS-DNA
  * @package DomainNameApi
- * @version 2.2.0
+ * @version 2.2.3
  */
 
 use \WHMCS\Domain\TopLevel\ImportItem;
@@ -20,7 +20,7 @@ new DomainNameApi\Services\Language();
 
 function domainnameapi_version(): string
 {
-    return '2.2.2';
+    return '2.2.3';
 }
 
 function domainnameapi_getConfigArray($params) {
