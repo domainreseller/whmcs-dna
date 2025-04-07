33c346aea87b92630f4b43c78510c672314c2f95
xa81 <poisonkid@gmail.com>
2025-04-08 02:32:47 +0300

    bump version to 2.2.7 in domainnameapi.php and whmcs.json

--- modules/registrars/domainnameapi/domainnameapi.php
+++ modules/registrars/domainnameapi/domainnameapi.php
@@ -2,7 +2,7 @@
 /**
  * Module WHMCS-DNA
  * @package DomainNameApi
- * @version 2.2.6
+ * @version 2.2.7
  */
 
 use \WHMCS\Domain\TopLevel\ImportItem;
@@ -20,7 +20,7 @@ new DomainNameApi\Services\Language();
 
 function domainnameapi_version(): string
 {
-    return '2.2.6';
+    return '2.2.7';
 }
 
 function domainnameapi_getConfigArray($params) {
