c898b0c6a466ed901d908219199eebcc09f616f0
xa81 <poisonkid@gmail.com>
2026-06-02 02:11:24 +0300

    Bump version to 3.0.4

--- modules/registrars/domainnameapi/domainnameapi.php
+++ modules/registrars/domainnameapi/domainnameapi.php
@@ -2,7 +2,7 @@
 /**
  * Module WHMCS-DNA
  * @package DomainNameApi
- * @version 3.0.3
+ * @version 3.0.4
  */
 
 use \WHMCS\Domain\TopLevel\ImportItem;
@@ -20,7 +20,7 @@ new DomainNameApi\Services\Language();
 
 function domainnameapi_version(): string
 {
-    return '3.0.3';
+    return '3.0.4';
 }
 
 function domainnameapi_getConfigArray($params) {
